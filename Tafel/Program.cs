f0f7937f4eebbbafcba50541a68a63f552d755d3
Brent Flameyn <flameynbrent@leerling.sint-rembert.be>
2025-02-06 09:21:30 +0100

    oefening tafel gemaakt

--- Tafel/Program.cs
+++ Tafel/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"1*411={1 *411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"2*411={2 * 411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"3*411={3 * 411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"4*411={4 * 411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"5*411={5 * 411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"6*411={6 * 411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"7*411={7 * 411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"8*411={8 * 411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"9*411={9 * 411}");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"10*411={10 * 411}");
+            Console.ReadLine();
+            Console.Clear();
         }
     }
 }
